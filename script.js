732c4635ce0849b5a1a797da481069ac69fba9ec
Muhammad Nurul Momen <momeniiuc7@gmail.com>
2024-09-18 05:13:00 +0600

    push

---
+++ script.js
@@ -0,0 +1,18 @@
+let output;
+// output= document.all.length;
+// output = document.all[10];
+
+output = document.documentElement;
+output = document.head;
+output = document.head.children;
+
+output = document.doctype;
+output = document.domain;
+output = document.URL;
+output = document.characterSet;
+output = document.contentType;
+output = document.forms;
+
+
+
+console.log(output);
